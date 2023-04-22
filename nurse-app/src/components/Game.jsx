708f8d0bb26beed2483fba3852c4dbfc096557cf
Miles136 <nemanuel.montoya@gmail.com>
2023-04-21 21:16:33 -0400

    Minor update to Game, Added Game.jsx

---
+++ nurse-app/src/components/Game.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function UnityComponent() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/FitnessGame.loader.js",
+    dataUrl: "build/webgl.data",
+    frameworkUrl: "build/build.framework.js",
+    codeUrl: "build/build.wasm"
+  });
+
+  return (
+    <Unity unityProvider={unityProvider} style={{ width: 960, height: 540 }} />
+  );
+}
+
+export default UnityComponent;
